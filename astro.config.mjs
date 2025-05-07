bfc4a570e549124549cdd3e4a70a750a7f617a2b
KendallRey <107897863+KendallRey@users.noreply.github.com>
2025-05-07 16:37:40 +0800

    setup-i18n - setup inlang and sample messages

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,9 +1,23 @@
 // @ts-check
 import { defineConfig } from 'astro/config';
+import { paraglideVitePlugin } from '@inlang/paraglide-js';
+import node from "@astrojs/node";
 
 import react from '@astrojs/react';
 
 // https://astro.build/config
 export default defineConfig({
-  integrations: [react()]
+  integrations: [react()],
+  vite: {
+    plugins: [
+      paraglideVitePlugin({
+        project: "./project.inlang",
+        outdir: "./src/paraglide",
+      }),
+    ]
+  },
+  output: "server",
+  adapter: node({
+    mode: "standalone"
+  })
 });
