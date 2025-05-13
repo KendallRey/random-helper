92949197661e7e59b9cc278f8c5dcde013a4b7af
KendallRey <107897863+KendallRey@users.noreply.github.com>
2025-05-13 13:33:59 +0800

    netlify - added integration package and plugin

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,9 +1,10 @@
 // @ts-check
 import { defineConfig } from "astro/config";
 import { paraglideVitePlugin } from "@inlang/paraglide-js";
-import node from "@astrojs/node";
 
 import react from "@astrojs/react";
+
+import netlify from "@astrojs/netlify";
 
 // https://astro.build/config
 export default defineConfig({
@@ -17,7 +18,5 @@ export default defineConfig({
     ],
   },
   output: "server",
-  adapter: node({
-    mode: "standalone",
-  }),
+  adapter: netlify(),
 });
